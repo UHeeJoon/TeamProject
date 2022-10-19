2e82f17f9ff2f2410b027fb476e8863e033d41f8
희준 유 <yhj3855@naver.com>
2022-10-19 18:53:36 +0900

    수정

--- cafeKiosk/src/cafeKioskMain/KioskMain.java
+++ cafeKiosk/src/cafeKioskMain/KioskMain.java
@@ -3,6 +3,7 @@ package cafeKioskMain;
 import java.util.HashMap;
 import java.util.Scanner;
 
+import view.AdminView;
 import view.PrintMenu;
 
 public class KioskMain {
@@ -17,46 +18,69 @@ public class KioskMain {
 		String isContinue = null;
 		HashMap<String, Integer> orderHistory = new HashMap<>();
 		// 변수 선언부 끝
-		
-		System.out.println("\n==============================================================================================================");
-		System.out.println("==============================================================================================================");
-		System.out.println("\t||    ||     //\\\\   ||||||||||   ||||||			||||||||     //\\\\     ||||||||  ||||||||");
-		System.out.println("\t||    ||    //  \\\\      ||	 ||                     ||          //  \\\\    ||        ||      ");
-		System.out.println("\t||||||||   ||    ||     ||       ||||||    		||         ///||\\\\\\   ||||||||  ||||||||");
-		System.out.println("\t||    ||    \\\\  //      ||       ||  ||			||        //      \\\\  ||        ||      ");
-		System.out.println("\t||    ||     \\\\//       ||       ||||||			|||||||| //        \\\\ ||        ||||||||");
-		System.out.println("==============================================================================================================");
-		System.out.println("==============================================================================================================");
+
+		LogoPrint();
 
 		while (true) {
-			new PrintMenu().print();
-			System.out.println("주문하시겠습니까?(y/n)");
-			select = sc.nextLine();
+			System.out.println(
+						""
+						+ "1.주문하기\n" 
+						+ "2.관리자 화면으로 가기");
 			
-			if(select.equals("n") || select.equals("N"))
-				break;
-			else if(select.equals("y") || select.equals("Y")){
-				do {
-				System.out.println("메뉴 명 : ");
-				menuName = sc.nextLine();
-				System.out.println("수  량 : ");
-				menuCnt = sc.nextLine();
-				orderHistory.put(menuName, Integer.valueOf(menuCnt));
-				System.out.println("추가 주문 하시겠습니까?(y/n)");
-				isContinue = sc.nextLine();
-				
-				}while(isContinue.equals("y") || isContinue.equals("Y"));
+			if ((sc.nextLine()).equals("1")) {
+				new PrintMenu().print();
+				System.out.println(">> 주문하시겠습니까?(y/n)");
+				select = sc.nextLine();
+
+				if (select.equals("n") || select.equals("N"))
+					break;
+				else if (select.equals("y") || select.equals("Y")) {
+					System.out.println("☆★☆★☆★☆★주 문☆★☆★☆★☆★");
+					do {
+						System.out.println("메뉴 명 : ");
+						menuName = sc.nextLine();
+						System.out.println("수  량 : ");
+						menuCnt = sc.nextLine();
+						orderHistory.put(menuName, Integer.valueOf(menuCnt));
+						System.out.println("추가 주문 하시겠습니까?(y/n)");
+						isContinue = sc.nextLine();
+
+					} while (isContinue.equals("y") || isContinue.equals("Y"));
+				} else {
+					System.out.println("※※※※입력 오류※※※※※");
+					try {
+						Thread.sleep(2000);
+					} catch (InterruptedException e) {
+					}
+				}
 			}
 			else {
-				System.out.println("※※※※입력 오류※※※※※");
-				try {
-					Thread.sleep(2000);
-			    } catch (InterruptedException e) { }
+				new AdminView().print();
 			}
 		}
 		System.out.println("종료");
 
 		sc.close();
+	}
+
+	public static void LogoPrint() {
+		String[] logo = {
+				"\n==============================================================================================================\n"
+						+ "==============================================================================================================",
+				"\t||    ||     //\\\\   ||||||||||   ||||||			||||||||     //\\\\     ||||||||  ||||||||",
+				"\t||    ||    //  \\\\      ||	 ||                     ||          //  \\\\    ||        ||      ",
+				"\t||||||||   ||    ||     ||       ||||||    		||         ///||\\\\\\   ||||||||  ||||||||",
+				"\t||    ||    \\\\  //      ||       ||  ||			||        //      \\\\  ||        ||      ",
+				"\t||    ||     \\\\//       ||       ||||||			|||||||| //        \\\\ ||        ||||||||",
+				"==============================================================================================================\n"
+						+ "==============================================================================================================\n" };
+		for (String str : logo) {
+			System.out.println(str);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+			}
+		}
 	}
 
 }
